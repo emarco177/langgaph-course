9daa9189397622c488690d09bcb32cf8bd9ea49d
Alonso Isidoro Román <alonsoir@gmail.com>
2024-05-30 11:44:39 +0200

    working with local and remote neo4j servers and llm.

--- knowledge-graph-rag.py
+++ knowledge-graph-rag.py
@@ -18,6 +18,7 @@ from langchain.agents import AgentType
 
 load_dotenv()
 
+
 def create_pinecone_index(index_name, pc):
     index = pc.Index(index_name)
     if index.exists:
@@ -127,6 +128,149 @@ def export_graph_to_csv(graph, output_dir):
             f.write(f"\n{source_id},{target_id},{rel_type}")
 
 
+def neo4j_local_llm():
+    # Connect to a Neo4j database
+    url = "bolt://localhost:7687"
+    username = "neo4j"
+    password = "password"
+    graph = Neo4jGraph(
+        url=url,
+        username=username,
+        password=password
+    )
+    data_url = "https://gist.githubusercontent.com/tomasonjo/08dc8ba0e19d592c4c3cde40dd6abcc3/raw/da8882249af3e819a80debf3160ebbb3513ee962/microservices.json"
+    import_query = requests.get(data_url).json()['query']
+    graph.query(import_query)
+    # Crear una transacción de escritura
+    """
+    tx = graph.begin(readonly=False)
+
+    try:
+        # Ejecutar la consulta de importación dentro de la transacción
+        tx.run(import_query)
+        # Confirmar la transacción
+        tx.commit()
+    except Exception as e:
+        # Si ocurre un error, revertir la transacción
+        tx.rollback()
+        print(f"Error al ejecutar la consulta: {e}")
+    """
+    print("local graph imported")
+
+    vector_index = Neo4jVector.from_existing_graph(
+        OpenAIEmbeddings(),
+        url=url,
+        username=username,
+        password=password,
+        index_name='tasks',
+        node_label="Task",
+        text_node_properties=['name', 'description', 'status'],
+        embedding_node_property='embedding',
+    )
+    query = "How will RecommendationService be updated?"
+    print(f"query is {query}")
+    response = vector_index.similarity_search(query)
+    for result in response:
+        print(result.page_content)
+    vector_qa = RetrievalQA.from_chain_type(
+        llm=ChatOpenAI(),
+        chain_type="stuff",
+        retriever=vector_index.as_retriever()
+    )
+    query = "How will recommendation service be updated?"
+    print(f"query is {query}")
+    response = vector_qa.invoke(query)
+    print(response)
+    query = "How many open tickets are there?"
+    print(f"query is {query}")
+    response = vector_qa.invoke(query)
+    print(response)
+    query = "MATCH (t:Task {status:'Open'}) RETURN count(*)"
+    print(f"query is {query}")
+    response = vector_qa.invoke(query)
+    print(response)
+    # Un grafo corriendo en local no tiene método refresh_schema
+    graph.refresh_schema()
+
+    # Crear una cadena Cypher QA
+    cypher_llm = ChatOpenAI(temperature=0)
+    qa_llm = ChatOpenAI(temperature=0)
+    cypher_chain = GraphCypherQAChain.from_llm(
+        cypher_llm=cypher_llm,
+        qa_llm=qa_llm,
+        graph=graph,
+        verbose=True
+    )
+
+    query = "How many open tickets there are?"
+    print(f"query is {query}")
+    response = cypher_chain.invoke(query)
+    print(response)
+    query = "Which team has the most open tasks?"
+    print(f"query is {query}")
+    response = cypher_chain.invoke(query)
+    print(response)
+    query = "Which services depend on Database directly?"
+    print(f"query is {query}")
+    response = cypher_chain.invoke(query)
+    print(response)
+    query = "Which services depend on Database indirectly?"
+    print(f"query is {query}")
+    response = cypher_chain.invoke(query)
+    print(response)
+
+    tools = [
+        Tool(
+            name="Tasks",
+            func=vector_qa.run,
+            description="""Useful when you need to answer questions about descriptions of tasks.
+                Not useful for counting the number of tasks.
+                Use full question as input.
+                """,
+        ),
+        Tool(
+            name="Graph",
+            func=cypher_chain.run,
+            description="""Useful when you need to answer questions about microservices,
+                their dependencies or assigned people. Also useful for any sort of 
+                aggregation like counting the number of tasks, etc.
+                Use full question as input.
+                """,
+        ),
+    ]
+    # Crear un ChatPromptTemplate
+    prompt_template = ChatPromptTemplate.from_messages([
+        ("system", "You are a helpful AI assistant. Use the provided tools to answer the user's questions."),
+        ("user", "{input}"),
+        ("assistant", "{agent_scratchpad}")
+    ])
+    # Inicializar el modelo LLM correctamente
+    llm = ChatOpenAI(temperature=0, model_name='gpt-4')
+    """
+    # Crear el agente usando el nuevo método
+    mrkl = create_openai_functions_agent(
+        tools=tools,
+        llm=llm,
+        prompt=prompt_template
+    )
+
+    """
+    mrkl = initialize_agent(
+        tools,
+        llm,
+        agent=AgentType.OPENAI_FUNCTIONS, verbose=True
+    )
+
+    query = "Which team is assigned to maintain PaymentService?"
+    print(f"query is {query}")
+    response = mrkl.invoke(query)
+    print(response)
+    query = "Which tasks have optimization in their description?"
+    print(f"query is {query}")
+    response = mrkl.invoke(query)
+    print(response)
+
+
 def neo4j_local():
     # Connect to a Neo4j database
     graph = Graph("bolt://localhost:7687", auth=("neo4j", "password"))
@@ -174,7 +318,6 @@ def neo4j_remote():
     url = os.getenv("remote_neo4j_url")
     username = os.getenv("remote_neo4j_username")
     password = os.getenv("remote_neo4j_password")
-
 
     graph = Neo4jGraph(
         url=url,
@@ -301,6 +444,7 @@ if __name__ == "__main__":
     la funcion local está basada en 
     https://sunila-gollapudi.medium.com/using-knowledge-graphs-to-enhance-retrieval-augmented-generation-rag-systems-14197efc1bab
     '''
+    neo4j_local_llm()
     neo4j_local()
     neo4j_remote()
     print("Done")
