2415ad2710a112fd735020a2f99f073d0483b905
Eden Marco <emarco177@gmail.com>
2025-04-22 12:44:25 +0300

    Add react.py for React agent implementation with tools and prompt integration

---
+++ react.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+from langchain import hub
+from langchain.agents import create_react_agent
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import tool
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+
+react_prompt: PromptTemplate = hub.pull("hwchase17/react")
+
+
+@tool
+def triple(num: float) -> float:
+    """
+    :param num: a number to triple
+    :return: the number tripled ->  multiplied by 3
+    """
+    return 3 * float(num)
+
+
+tools = [TavilySearch(max_results=1), triple]
+
+llm = ChatOpenAI(model="o4-mini")
+
+react_agent_runnable = create_react_agent(llm, tools, react_prompt)
