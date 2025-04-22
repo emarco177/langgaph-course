bb79ec6f76bfe2c43e6ed275230915765ef40f4f
Eden Marco <emarco177@gmail.com>
2025-04-22 06:01:43 +0300

    Implement message graph for AI tool execution and revision process
    - Refactored main.py to establish a MessageGraph for managing tool execution and revisions.
    - Added event_loop function to control iteration limits for tool visits.
    - Integrated first_responder, execute_tools, and revisor as nodes in the graph.
    - Set up conditional edges and entry point for the graph, enabling dynamic tool invocation.
    - Enhanced output to display the generated graph and results of the AI-powered task.

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
-from dotenv import load_dotenv
+from typing import List
 
-load_dotenv()
+from langchain_core.messages import BaseMessage, ToolMessage
+from langgraph.graph import END, MessageGraph
+
+from chains import revisor, first_responder
+from tool_executor import execute_tools
+
+MAX_ITERATIONS = 2
+builder = MessageGraph()
+builder.add_node("draft", first_responder)
+builder.add_node("execute_tools", execute_tools)
+builder.add_node("revise", revisor)
+builder.add_edge("draft", "execute_tools")
+builder.add_edge("execute_tools", "revise")
 
 
-if __name__ == "__main__":
-    print("Hello Reflexion")
+def event_loop(state: List[BaseMessage]) -> str:
+    count_tool_visits = sum(isinstance(item, ToolMessage) for item in state)
+    num_iterations = count_tool_visits
+    if num_iterations > MAX_ITERATIONS:
+        return END
+    return "execute_tools"
+
+
+builder.add_conditional_edges("revise", event_loop)
+builder.set_entry_point("draft")
+graph = builder.compile()
+
+print(graph.get_graph().draw_mermaid())
+
+
+res = graph.invoke(
+    "Write about AI-Powered SOC / autonomous soc  problem domain, list startups that do that and raised capital."
+)
+print(res[-1].tool_calls[0]["args"]["answer"])
+print(res)
