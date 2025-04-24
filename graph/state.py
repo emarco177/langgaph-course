03f79ae97fa601a7cd8b8abc52855a6f8095d867
Eden Marco <emarco177@gmail.com>
2025-04-24 13:54:04 +0300

    4-state

---
+++ graph/state.py
@@ -0,0 +1,18 @@
+from typing import List, TypedDict
+
+
+class GraphState(TypedDict):
+    """
+    Represents the state of our graph.
+
+    Attributes:
+        question: question
+        generation: LLM generation
+        web_search: whether to add search
+        documents: list of documents
+    """
+
+    question: str
+    generation: str
+    web_search: bool
+    documents: List[str]
