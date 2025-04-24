c2d71c79a8ab15d0e619bb7ca5adc75df49143de
Eden Marco <emarco177@gmail.com>
2025-04-24 13:55:08 +0300

    5-retreive-node

---
+++ graph/nodes/retrieve.py
@@ -0,0 +1,12 @@
+from typing import Any, Dict
+
+from graph.state import GraphState
+from ingestion import retriever
+
+
+def retrieve(state: GraphState) -> Dict[str, Any]:
+    print("---RETRIEVE---")
+    question = state["question"]
+
+    documents = retriever.invoke(question)
+    return {"documents": documents, "question": question}
