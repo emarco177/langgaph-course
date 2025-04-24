ed491a688010fdad864fc561da203cb25e7a4855
Eden Marco <emarco177@gmail.com>
2025-04-24 06:12:40 +0300

    Add message graph implementation in main.py for handling generation and reflection nodes, including conditional logic and example input for tweet enhancement.

--- main.py
+++ main.py
@@ -1,7 +1,55 @@
-from dotenv import load_dotenv
+from typing import List, Sequence
 
+from dotenv import load_dotenv
 load_dotenv()
 
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, MessageGraph
+
+from chains import generate_chain, reflect_chain
+
+
+REFLECT = "reflect"
+GENERATE = "generate"
+
+
+def generation_node(state: Sequence[BaseMessage]):
+    return generate_chain.invoke({"messages": state})
+
+
+def reflection_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
+    res = reflect_chain.invoke({"messages": messages})
+    return [HumanMessage(content=res.content)]
+
+
+builder = MessageGraph()
+builder.add_node(GENERATE, generation_node)
+builder.add_node(REFLECT, reflection_node)
+builder.set_entry_point(GENERATE)
+
+
+def should_continue(state: List[BaseMessage]):
+    if len(state) > 6:
+        return END
+    return REFLECT
+
+
+builder.add_conditional_edges(GENERATE, should_continue)
+builder.add_edge(REFLECT, GENERATE)
+
+graph = builder.compile()
+print(graph.get_graph().draw_mermaid())
+graph.get_graph().print_ascii()
 
 if __name__ == "__main__":
     print("Hello LangGraph")
+    inputs = HumanMessage(content="""Make this tweet better:"
+                                    @LangChainAI
+            — newly Tool Calling feature is seriously underrated.
+
+            After a long wait, it's  here- making the implementation of agents across different models with function calling - super easy.
+
+            Made a video covering their newest blog post
+
+                                  """)
+    response = graph.invoke(inputs)
