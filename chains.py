d2f15d26d7355b615ff9ab32a9e770505cf11aff
Eden Marco <emarco177@gmail.com>
2025-04-24 06:11:16 +0300

    Add chains.py to implement Twitter influencer prompts and generation logic

---
+++ chains.py
@@ -0,0 +1,30 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+
+reflection_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a viral twitter influencer grading a tweet. Generate critique and recommendations for the user's tweet."
+            "Always provide detailed recommendations, including requests for length, virality, style, etc.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+generation_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a twitter techie influencer assistant tasked with writing excellent twitter posts."
+            " Generate the best twitter post possible for the user's request."
+            " If the user provides critique, respond with a revised version of your previous attempts.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+
+llm = ChatOpenAI()
+generate_chain = generation_prompt | llm
+reflect_chain = reflection_prompt | llm
